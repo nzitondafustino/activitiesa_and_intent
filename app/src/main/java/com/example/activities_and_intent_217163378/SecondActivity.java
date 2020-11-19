47bf8c4ea00cf2ea39723ee4db242ce8915f1b24
=nzitindafustino <=nzitindafustino@gmail.com>
2020-11-19 16:13:24 +0200

    activities and intent

---
+++ app/src/main/java/com/example/activities_and_intent_217163378/SecondActivity.java
@@ -0,0 +1,33 @@
+package com.example.activities_and_intent_217163378;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+        mReply = findViewById(R.id.editText_second);
+    }
+    public void returnReply(View v){
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+}
